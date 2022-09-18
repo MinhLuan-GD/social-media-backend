6e839a13d18edee9c55b36a890bd233e8f4759e2
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-09-18 20:16:33 +0700

    #9 Update reacts module file

--- src/reacts/reacts.module.ts
+++ src/reacts/reacts.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ReactsService } from './reacts.service';
 import { ReactsController } from './reacts.controller';
+import { User, UserSchema } from '@users/schemas/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { React, ReactSchema } from './schemas/react.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: React.name, schema: ReactSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   providers: [ReactsService],
   controllers: [ReactsController],
 })
