02b4e1b38bd79293ec754addfa24fbceba085a5a
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-09-18 15:48:49 +0700

    #5 Update user module file

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+import { Post, PostSchema } from '@/posts/schemas/post.schema';
+import { userCacheOpts } from '@/cache';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Post.name, schema: PostSchema },
+    ]),
+    CacheModule.registerAsync(userCacheOpts),
+  ],
   providers: [UsersService],
   controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
