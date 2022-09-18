9fdaf3b745e9b6b15f716a8b4c3125936f1b0da3
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-09-18 19:58:01 +0700

    #7 Update posts module file

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { User, UserSchema } from '@users/schemas/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostSchema } from './schemas/post.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Post.name, schema: PostSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   providers: [PostsService],
   controllers: [PostsController],
 })
