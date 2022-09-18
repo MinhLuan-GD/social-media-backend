6012afb858accc3072e6e08e29684e6e47594ffc
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-09-18 16:07:46 +0700

    #5 Update 2209181607

---
+++ src/auth/auth.util.ts
@@ -0,0 +1,105 @@
+import { UsersService } from '@users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { OAuth2Client } from 'google-auth-library';
+import { MailerService } from '@nestjs-modules/mailer';
+import { MyLogger } from '@/logger/logger.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthUtil {
+  constructor(
+    public usersService: UsersService,
+    public jwtService: JwtService,
+    private mailerService: MailerService,
+  ) {
+    this.myOAuth2Client.setCredentials({
+      refresh_token: process.env.MAILING_REFRESH,
+    });
+  }
+
+  private rexa = /[áàảãạăắằẳẵặâấầẩẫậ]/gi;
+  private rexe = /[éèẻẽẹêếềểễệ]/gi;
+  private rexi = /[íìỉĩị]/gi;
+  private rexu = /[úùủũụưứừửữự]/gi;
+  private rexo = /[óòỏõọôốồổỗộơớờởỡợ]/gi;
+  private c = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  private cLen = this.c.length;
+  private schema = '0123456789';
+  private logger = new MyLogger();
+  private myOAuth2Client = new OAuth2Client(
+    process.env.MAILING_ID,
+    process.env.MAILING_SECRET,
+  );
+
+  async gUsername(username: string) {
+    const baseUsername = this.convert(username);
+    username = this.makeId(baseUsername);
+    while (true)
+      if (await this.usersService.findByUsername(username))
+        username = this.makeId(baseUsername);
+      else return username;
+  }
+
+  async sendVerificationEmail(email: string, name: string, url: string) {
+    const myAccessTokenObject = await this.myOAuth2Client.getAccessToken();
+    const myAccessToken = myAccessTokenObject?.token;
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: 'Email verification',
+      html: `Hello ${name}, link: ${url}`,
+      auth: {
+        refreshToken: process.env.MAILING_REFRESH,
+        accessToken: myAccessToken,
+      },
+    };
+    this.mailerService
+      .sendMail(mailOptions)
+      .then((val) => val)
+      .catch((res) => this.logger.error(res));
+  }
+
+  async sendResetCode(email: string, name: string, code: string) {
+    const myAccessTokenObject = await this.myOAuth2Client.getAccessToken();
+    const myAccessToken = myAccessTokenObject?.token;
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: 'Email reset password',
+      html: `Hello ${name}, your code: ${code}`,
+      auth: {
+        refreshToken: process.env.MAILING_REFRESH,
+        accessToken: myAccessToken,
+      },
+    };
+    this.mailerService
+      .sendMail(mailOptions)
+      .then((val) => val)
+      .catch((res) => this.logger.error(res));
+  }
+
+  generateCode(length: number) {
+    let code = '';
+    for (let i = 0; i < length; i++)
+      code += this.schema.charAt(
+        Math.floor(Math.random() * this.schema.length),
+      );
+
+    return code;
+  }
+
+  private makeId(username: string) {
+    username += '_';
+    for (let i = 0; i < 9; i++)
+      username += this.c.charAt(Math.floor(Math.random() * this.cLen));
+    return username;
+  }
+
+  private convert = (text: string) =>
+    text
+      .replace(this.rexa, 'a')
+      .replace(this.rexe, 'e')
+      .replace(this.rexo, 'o')
+      .replace(this.rexi, 'i')
+      .replace(this.rexu, 'u');
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UpdateOptions } from './interfaces/opts.interface';
+import { User, UserDocument } from './schemas/user.schema';
+import { Cache } from 'cache-manager';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  @InjectModel(User.name)
+  private usersModel: Model<UserDocument>;
+
+  @Inject(CACHE_MANAGER)
+  private cacheManager: Cache;
+
+  async findByEmail(email: string): Promise<User & { _id: string }> {
+    return this.usersModel.findOne({ email }).lean();
+  }
+
+  async findByUsername(username: string): Promise<User & { _id: string }> {
+    return this.usersModel.findOne({ username }).lean();
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.usersModel.findById(id).lean();
+  }
+
+  async createUser(input: any): Promise<User & { _id: string }> {
+    const user = await this.usersModel.create(input);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...result }: User & { _id: string } = user.toJSON();
+    return result;
+  }
+
+  async updateUser(filter: any, data: any, options: UpdateOptions = {}) {
+    const user = await this.usersModel.findOneAndUpdate(filter, data, options);
+    return user;
+  }
+
+  async setCode(user: string, code: string, ttl = 120) {
+    await this.cacheManager.set(`users:${user}:code`, code, { ttl });
+  }
+
+  async getCode(user: string) {
+    const code = await this.cacheManager.get(`users:${user}:code`);
+    return code;
+  }
+}
