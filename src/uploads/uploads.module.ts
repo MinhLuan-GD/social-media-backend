22a3c865ca046cedf1ce03ef1fbf24ae59189bc1
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-09-18 21:15:09 +0700

    #11 Update uploads module file

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadsService } from './uploads.service';
 import { UploadsController } from './uploads.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [MulterModule.register({ dest: './tmp' })],
   providers: [UploadsService],
   controllers: [UploadsController],
 })
