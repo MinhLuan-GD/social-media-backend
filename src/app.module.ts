2fcc89c1669402739910f62ad2f379aaab46f527
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-09-18 15:17:17 +0700

    #3 Update app module file - 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { configOpts, mailOpts } from './config/async.config';
+import { mongoOpts } from './databases';
 import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [
+    ConfigModule.forRoot(configOpts),
+    MongooseModule.forRootAsync(mongoOpts),
+    MailerModule.forRootAsync(mailOpts),
+    LoggerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
