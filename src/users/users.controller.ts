af39f12047ee15a80608df94dee9683bf0ebbea0
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-09-18 16:17:06 +0700

    #5 Update 2209181617

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,163 @@
-import { Controller } from '@nestjs/common';
+import { RequestWithUser } from '@auth/interfaces/auth.interface';
+import { JwtAuthGuard } from '@auth/jwt-auth.guard';
+import {
+  Controller,
+  Get,
+  Param,
+  UseGuards,
+  Request,
+  Body,
+  Patch,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get('get-user-picture/:email')
+  async getUserPicture(@Param('email') email: string) {
+    return this.usersService.getUserPicture(email);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('get-profile/:username')
+  async getProfile(
+    @Request() req: RequestWithUser,
+    @Param('username') username: string,
+  ) {
+    return this.usersService.getProfile(req.user._id, username);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('update-profile-picture')
+  async updateProfilePicture(
+    @Request() req: RequestWithUser,
+    @Body('url') url: string,
+  ) {
+    this.usersService.updateUser({ _id: req.user._id }, { picture: url });
+    return url;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('update-cover')
+  async updateCover(@Request() req: RequestWithUser, @Body('url') url: string) {
+    this.usersService.updateUser({ _id: req.user._id }, { cover: url });
+    return url;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('update-details')
+  async updateDetails(
+    @Request() req: RequestWithUser,
+    @Body('infos') infos: string,
+  ) {
+    const { details } = await this.usersService.updateUser(
+      { _id: req.user._id },
+      { details: infos },
+      { new: true },
+    );
+    return details;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('add-friend/:id')
+  async addFriend(
+    @Request() req: RequestWithUser,
+    @Param('id') receiverId: string,
+  ) {
+    return this.usersService.addFriend(req.user._id, receiverId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('cancel-request/:id')
+  async cancelRequest(
+    @Request() req: RequestWithUser,
+    @Param('id') receiverId: string,
+  ) {
+    return this.usersService.cancelRequest(req.user._id, receiverId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('follow/:id')
+  async follow(
+    @Request() req: RequestWithUser,
+    @Param('id') receiverId: string,
+  ) {
+    return this.usersService.follow(req.user._id, receiverId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('unfollow/:id')
+  async unfollow(
+    @Request() req: RequestWithUser,
+    @Param('id') receiverId: string,
+  ) {
+    return this.usersService.unfollow(req.user._id, receiverId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('accept-request/:id')
+  async acceptRequest(
+    @Request() req: RequestWithUser,
+    @Param('id') id: string,
+  ) {
+    return this.usersService.acceptRequest(id, req.user._id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('unfriend/:id')
+  async unfriend(
+    @Request() req: RequestWithUser,
+    @Param('id') receiverId: string,
+  ) {
+    return this.usersService.unfriend(req.user._id, receiverId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('delete-request/:id')
+  async deleteRequest(
+    @Request() req: RequestWithUser,
+    @Param('id') id: string,
+  ) {
+    return this.usersService.deleteRequest(id, req.user._id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('search/:searchTerm')
+  async search(@Param('searchTerm') searchTerm: string) {
+    // console.log(`searching:::${searchTerm}`);
+    return this.usersService.search(searchTerm);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('add-to-search-history')
+  async addToSearchHistory(
+    @Request() req: RequestWithUser,
+    @Body('searchUser') searchUser: string,
+  ) {
+    return this.usersService.addToSearchHistory(req.user._id, searchUser);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('get-search-history')
+  async getSearchHistory(@Request() req: RequestWithUser) {
+    return this.usersService.getSearchHistory(req.user._id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('remove-from-search')
+  async removeFromSearch(
+    @Request() req: RequestWithUser,
+    @Body('searchUser') searchUser: string,
+  ) {
+    return this.usersService.removeFromSearch(req.user._id, searchUser);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('get-friends-page-infos')
+  async getFriendsPageInfos(@Request() req: RequestWithUser) {
+    return this.usersService.getFriendsPageInfos(req.user._id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
-import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Post, PostDocument } from '@posts/schemas/post.schema';
+import {
+  CACHE_MANAGER,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UpdateOptions } from './interfaces/opts.interface';
@@ -9,6 +16,9 @@ import { Cache } from 'cache-manager';
 export class UsersService {
   @InjectModel(User.name)
   private usersModel: Model<UserDocument>;
+
+  @InjectModel(Post.name)
+  private postsModel: Model<PostDocument>;
 
   @Inject(CACHE_MANAGER)
   private cacheManager: Cache;
@@ -44,5 +54,268 @@ export class UsersService {
   async getCode(user: string) {
     const code = await this.cacheManager.get(`users:${user}:code`);
     return code;
+  }
+
+  async getUserPicture(email: string) {
+    const user = await this.usersModel.findOne({ email }).lean();
+    if (!user) {
+      throw new HttpException(
+        'The email address you entered is not connected to an account.',
+        HttpStatus.CONFLICT,
+      );
+    }
+    return { email, picture: user.picture };
+  }
+
+  async getProfile(id: string, username: string) {
+    const user = await this.usersModel.findById(id);
+    const profile = await this.usersModel
+      .findOne({ username })
+      .select('-password');
+    const friendship = {
+      friends: false,
+      following: false,
+      requestSent: false,
+      requestReceived: false,
+    };
+    if (!profile) throw new HttpException('Conflict', HttpStatus.CONFLICT);
+
+    if (
+      user.friends.includes(profile._id) &&
+      profile.friends.includes(user._id)
+    )
+      friendship.friends = true;
+    if (user.following.includes(profile._id)) friendship.following = true;
+    if (user.requests.includes(profile._id)) friendship.requestReceived = true;
+    if (profile.requests.includes(user._id)) friendship.requestSent = true;
+
+    const posts = await this.postsModel
+      .find({ user: profile._id })
+      .populate('user')
+      .populate('comments.commentBy', 'first_name last_name username picture')
+      .sort({ createdAt: -1 });
+    await profile.populate('friends', 'first_name last_name username picture');
+    return {
+      friendship,
+      posts,
+      ...profile.toObject(),
+    };
+  }
+
+  async addFriend(senderId: string, receiverId: string) {
+    if (senderId !== receiverId) {
+      const sender = await this.usersModel.findById(senderId);
+      const receiver = await this.usersModel.findById(receiverId);
+
+      if (
+        !receiver.requests.includes(sender._id) &&
+        !receiver.friends.includes(sender._id)
+      ) {
+        await receiver.updateOne({ $push: { requests: sender._id } });
+        await receiver.updateOne({ $push: { followers: sender._id } });
+        await sender.updateOne({ $push: { following: receiver._id } });
+        return 'friend request has been sent';
+      } else throw new HttpException('Already sent', HttpStatus.CONFLICT);
+    } else {
+      throw new HttpException(
+        `You can't send a request to yourself`,
+        HttpStatus.CONFLICT,
+      );
+    }
+  }
+
+  async cancelRequest(senderId: string, receiverId: string) {
+    if (senderId !== receiverId) {
+      const sender = await this.usersModel.findById(senderId);
+      const receiver = await this.usersModel.findById(receiverId);
+
+      if (
+        receiver.requests.includes(sender._id) &&
+        !receiver.friends.includes(sender._id)
+      ) {
+        await receiver.updateOne({ $pull: { requests: sender._id } });
+        await receiver.updateOne({ $pull: { followers: sender._id } });
+        await sender.updateOne({ $pull: { following: receiver._id } });
+        return 'you successfully canceled request';
+      } else throw new HttpException('Already Canceled', HttpStatus.CONFLICT);
+    } else {
+      throw new HttpException(
+        `You can't cancel a request to yourself`,
+        HttpStatus.CONFLICT,
+      );
+    }
+  }
+
+  async follow(senderId: string, receiverId: string) {
+    if (senderId !== receiverId) {
+      const sender = await this.usersModel.findById(senderId);
+      const receiver = await this.usersModel.findById(receiverId);
+
+      if (
+        !receiver.followers.includes(sender._id) &&
+        !sender.following.includes(receiver._id)
+      ) {
+        await receiver.updateOne({ $push: { followers: sender._id } });
+        await sender.updateOne({ $push: { following: receiver._id } });
+        return 'follow success';
+      } else throw new HttpException('Already following', HttpStatus.CONFLICT);
+    } else
+      throw new HttpException(`You can't follow yourself`, HttpStatus.CONFLICT);
+  }
+
+  async unfollow(senderId: string, receiverId: string) {
+    if (senderId !== receiverId) {
+      const sender = await this.usersModel.findById(senderId);
+      const receiver = await this.usersModel.findById(receiverId);
+
+      if (
+        receiver.followers.includes(sender._id) &&
+        sender.following.includes(receiver._id)
+      ) {
+        await receiver.updateOne({ $pull: { followers: sender._id } });
+        await sender.updateOne({ $pull: { following: receiver._id } });
+        return 'unfollow success';
+      } else
+        throw new HttpException('Already not following', HttpStatus.CONFLICT);
+    } else {
+      throw new HttpException(
+        `You can't unfollow yourself`,
+        HttpStatus.CONFLICT,
+      );
+    }
+  }
+
+  async acceptRequest(senderId: string, receiverId: string) {
+    if (senderId !== receiverId) {
+      const sender = await this.usersModel.findById(senderId);
+      const receiver = await this.usersModel.findById(receiverId);
+
+      if (receiver.requests.includes(sender._id)) {
+        await receiver.updateOne({
+          $push: { friends: sender._id, following: sender._id },
+        });
+        await sender.updateOne({
+          $push: { friends: receiver._id, followers: receiver._id },
+        });
+        await receiver.updateOne({ $pull: { requests: sender._id } });
+        return 'friend request accepted';
+      } else throw new HttpException('Already friends', HttpStatus.CONFLICT);
+    } else {
+      throw new HttpException(
+        `You can't accept a request from  yourself`,
+        HttpStatus.CONFLICT,
+      );
+    }
+  }
+
+  async unfriend(senderId: string, receiverId: string) {
+    if (senderId !== receiverId) {
+      const sender = await this.usersModel.findById(senderId);
+      const receiver = await this.usersModel.findById(receiverId);
+
+      if (
+        receiver.friends.includes(sender._id) &&
+        sender.friends.includes(receiver._id)
+      ) {
+        await receiver.updateOne({
+          $pull: {
+            friends: sender._id,
+            following: sender._id,
+            followers: sender._id,
+          },
+        });
+        await sender.updateOne({
+          $pull: {
+            friends: receiver._id,
+            following: receiver._id,
+            followers: receiver._id,
+          },
+        });
+        return 'unfriend request accepted';
+      } else
+        throw new HttpException('Already not friends', HttpStatus.CONFLICT);
+    } else {
+      throw new HttpException(
+        `You can't unfriend yourself`,
+        HttpStatus.CONFLICT,
+      );
+    }
+  }
+
+  async deleteRequest(senderId: string, receiverId: string) {
+    if (senderId !== receiverId) {
+      const sender = await this.usersModel.findById(senderId);
+      const receiver = await this.usersModel.findById(receiverId);
+
+      if (receiver.requests.includes(sender._id)) {
+        await receiver.updateOne({
+          $pull: { requests: sender._id, followers: sender._id },
+        });
+        await sender.updateOne({ $pull: { following: receiver._id } });
+        return 'delete request accepted';
+      } else
+        throw new HttpException('Already deleted friends', HttpStatus.CONFLICT);
+    } else
+      throw new HttpException(`You can't delete yourself`, HttpStatus.CONFLICT);
+  }
+
+  async search(searchTerm: string) {
+    const users = await this.usersModel
+      .find({ $text: { $search: searchTerm } })
+      .select('first_name last_name username picture')
+      .limit(5)
+      .lean();
+
+    return users;
+  }
+
+  async addToSearchHistory(id: string, searchUser: string) {
+    const search = {
+      user: searchUser,
+      createdAt: new Date(),
+    };
+    const user = await this.usersModel.findById(id);
+    const check = user.search.find((x) => x.user.toString() === searchUser);
+    if (check) {
+      await this.usersModel.updateOne(
+        { _id: id, 'search.user': searchUser },
+        {
+          $set: { 'search.$.createdAt': new Date() },
+        },
+      );
+    } else await user.updateOne({ $push: { search } });
+    return 'ok';
+  }
+
+  async getSearchHistory(id: string) {
+    const { search } = await this.usersModel
+      .findById(id)
+      .select('search')
+      .populate('search.user', 'first_name last_name username picture')
+      .lean();
+    return search;
+  }
+
+  async removeFromSearch(id: string, searchUser: string) {
+    await this.usersModel.updateOne(
+      { _id: id },
+      { $pull: { search: { user: searchUser } } },
+    );
+    return 'ok';
+  }
+
+  async getFriendsPageInfos(id: string) {
+    const { friends, requests } = await this.usersModel
+      .findById(id)
+      .select('friends requests')
+      .populate('friends', 'first_name last_name picture username')
+      .populate('requests', 'first_name last_name picture username')
+      .lean();
+    const sentRequests = await this.usersModel
+      .find({ requests: id })
+      .select('first_name last_name picture username')
+      .lean();
+
+    return { friends, requests, sentRequests };
   }
 }
