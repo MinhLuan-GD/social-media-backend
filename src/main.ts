243f08b990aa3ac631a654b5267e3e87b7750eed
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-09-18 15:20:17 +0700

    #3 Update main file - 1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { v2 } from 'cloudinary';
+import compression from 'compression';
+import { AppModule } from '@/app.module';
+import { corsOpts, compressionOpts, cloudinaryOpts } from '@config/sync.config';
+import { HttpExceptionFilter } from '@exceptions/http-exception.filter';
+import { MyLogger } from '@logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { logger: new MyLogger() });
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors(corsOpts);
+  app.use(compression(compressionOpts));
+  v2.config(cloudinaryOpts);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
