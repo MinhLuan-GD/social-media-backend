f9cc1150adb7ee64e397b982c38a38f06a4de8e7
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-09-21 21:54:37 +0700

    #17 Update 2209212154

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import {
+  Conversation,
+  ConversationSchema,
+} from './schemas/conversation.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Conversation.name, schema: ConversationSchema },
+    ]),
+  ],
   providers: [ChatService],
   controllers: [ChatController],
 })
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,21 @@
+import { User, UserDocument } from '@users/schemas/user.schema';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import {
+  Conversation,
+  ConversationDocument,
+} from './schemas/conversation.schema';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  @InjectModel(Conversation.name)
+  private conversationsModel: Model<ConversationDocument>;
+
+  @InjectModel(User.name)
+  private usersModel: Model<UserDocument>;
+
+  async findConversations(userId: string, search = '') {
+    const { friends } = await this.usersModel.findById(userId).lean();
+  }
+}
